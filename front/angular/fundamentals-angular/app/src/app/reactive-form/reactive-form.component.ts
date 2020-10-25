46233bb241cc2695be72392de70d77d5bcda0110
Rildo Alves <junior.alvesp99@gmail.com>
2020-10-25 09:35:35 -0300

    Adiciona validacao de confirmar senha

--- front/angular/fundamentals-angular/app/src/app/reactive-form/reactive-form.component.ts
+++ front/angular/fundamentals-angular/app/src/app/reactive-form/reactive-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators, AbstractControl } from '@angular/forms';
+import { FormGroup, FormControl, FormBuilder, Validators, AbstractControl } from '@angular/forms';
+import { comparePassword } from "./custom-validator.validator";
 
 @Component({
   selector: 'app-reactive-form',
@@ -9,41 +10,43 @@ import { FormGroup, FormControl, Validators, AbstractControl } from '@angular/fo
 
 export class ReactiveFormComponent implements OnInit {
 
-  formCadastro : FormGroup
+  formRegister : FormGroup
 
-  constructor() {
-  }
+  submitted = false;
+  regex = /^([0-9]{3}\.?[0-9]{3}\.?[0-9]{3}\-?[0-9]{2}|[0-9]{2}\.?[0-9]{3}\.?[0-9]{3}\/?[0-9]{4}\-?[0-9]{2})$/;
+
+  constructor(private formBuilder : FormBuilder) {}
 
   ngOnInit() {
-    this.formCadastro = new FormGroup({
-      nome: new FormControl('', [Validators.required, Validators.minLength(5), Validators.maxLength(150)]),
-      cpf: new FormControl('', [Validators.pattern(/^([0-9]{3}\.?[0-9]{3}\.?[0-9]{3}\-?[0-9]{2}|[0-9]{2}\.?[0-9]{3}\.?[0-9]{3}\/?[0-9]{4}\-?[0-9]{2})$/), Validators.required]),
-      idade: new FormControl('', [Validators.min(18), Validators.required]),
-      telefone: new FormControl('', Validators.required),
-      email: new FormControl('', [Validators.email, Validators.required]),
-      senha: new FormControl('', [Validators.minLength(8), Validators.required]),
-    confirmaSenha: new FormControl('', [Validators.minLength(8), Validators.required/*, validaSenha*/])
-    });
+    this.formRegister = this.formBuilder.group(
+      {
+        name: ["", [Validators.required, Validators.minLength(5), Validators.maxLength(150)]],
+        cpf: ["", [Validators.pattern(this.regex), Validators.required]],
+        age: ["", [Validators.min(18), Validators.required]],
+        phone: ["", Validators.required],
+        email: ["", [Validators.email, Validators.required]],
+        password: ["", [Validators.minLength(8), Validators.required]],
+        confirmPassword: ["", [Validators.minLength(8), Validators.required]]
+      },
+      {
+        validator: comparePassword("password", "confirmPassword")
+      }
+    );
   }
 
-  enviar(){
-    let dados = `
-      Nome: ${this.formCadastro.value.nome}
-      CPF: ${this.formCadastro.value.cpf}
-      Idade: ${this.formCadastro.value.idade}
-      Telefone: ${this.formCadastro.value.telefone}
-      Email: ${this.formCadastro.value.email}
-      Senha: ${this.formCadastro.value.senha}
-      ConfirmaSenha: ${this.formCadastro.value.confirmaSenha}
-    `;
+  // Necessaria pra retornar o valor dos FormControl do FormGroup
+  get f() {
+    return this.formRegister.value;
+  }
 
-    console.log(dados);
+  onSubmit(){
+    this.submitted = true;
+
+    // Retorna false se o form for invalido
+    if(this.formRegister.invalid) {
+      return;
+    }
+
+    console.log("Valores: " + JSON.stringify(this.formRegister.value));
   }
 }
-
-/*function validaSenha(control: AbstractControl) {
-  if(control.value !== control.parent.controls['senha'].value){
-    return {'senhaInvalida': true}
-  }
-  return null;
-}*/
